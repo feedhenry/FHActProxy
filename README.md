0c2581a4d84d21d637f15b9d059577044cc032a0
Gareth Murphy <gareth.cpm@gmail.com>
2012-09-19 13:13:52 +0100

    Minor doc cleanup and code changes. Nothing noteworthy.

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
 # Feedhenry Act Call Proxy
 
-FHActProxy is a Sencha User Extension to allow developers to perform a Feedhenry action call to the cloud through a special type of proxy known as 'fhact'. 
+FHActProxy is a Sencha User Extension to allow developers to perform a Feedhenry action call to the
+cloud through a special type of proxy known as 'fhactproxy'.
 
 ## Installation
+
 To use the FHActProxy, simply include the file in the head of your HTML: 
 
     <script type="text/javascript" src="js/FHActProxy.js"></script>
 
-This script should be included immediately after sencha-touch.js. 
+This script should be included immediately after sencha-touch.js. Alternatively, place it in your
+source directory and define it in your apps `Ext.Loader` config, as follows:
+
+    Ext.Loader.setConfig({
+        enabled : true,
+        paths   : {
+            'Ext.ux.FHActProxy': './path/to/file/fhactproxy.js'
+        }
+    });
 
 ## Usage
-Register a Sencha model for your store, referencing the 'fhact' proxy. The id is the name of the cloud action call that the proxy references. 
+
+Register a Sencha model for your store, referencing the 'fhactproxy' proxy. The id is the name of
+the cloud action call that the proxy references.
 
     Ext.define('listModel', {
       extend: "Ext.data.Model",
       config: {
         fields: ['name', 'phone'],
         proxy : {
-            type: 'fhact',
+            type: 'fhactproxy',
             reader: 'json',
             act: 'getNames' // the name of an act call (a function in the cloud)
         }
@@ -50,7 +62,9 @@ Here's an example cloud action call loading some data to work with this store:
       return { data: records};
     }
 
-*New:* You can also tell the proxy to send some data through with the proxy using the 'req' parameter. In our definition of the proxy (as above), as well as specifying the name of the act call to use, we could specify some JSON data to send through:
+*New:* You can also tell the proxy to send some data through with the proxy using the 'req'
+parameter. In our definition of the proxy (as above), as well as specifying the name of the act call
+to use, we could specify some JSON data to send through:
 
     {
       type: 'fhact',
@@ -59,7 +73,8 @@ Here's an example cloud action call loading some data to work with this store:
       req: { includePhotos: true, gender: 'male' }
     }
 
-A clever use of this is to send authentication data through with every request. If we set the req paramater to a string, it'll automatically look up $fh.data local storage and retrieve with that key.
+A clever use of this is to send authentication data through with every request. If we set the req
+paramater to a string, it'll automatically look up $fh.data local storage and retrieve with that key.
 So, if we store a session object in $fh.data after log in like so:
 
     var s = { session: 'a62b81abff3', token: 'f9f0jsefj903' };
@@ -69,9 +84,8 @@ So, if we store a session object in $fh.data after log in like so:
       value: JSON.stringify(s)
     });
 
-we can then tell our proxy to use this stored session data with all future request - since we pass a string, it'll automatically pull from $fh.data with the key that matches the value of 'request'.
-
-we can then tell our proxy to use this stored session data with all future request - since we pass a string, it'll automatically pull from $fh.data with the key that matches the value of 'request'.
+we can then tell our proxy to use this stored session data with all future request - since we pass
+a string, it'll automatically pull from $fh.data with the key that matches the value of 'request'.
 
     {
       type: 'fhact',
--- fhactproxy.js
+++ fhactproxy.js
@@ -1,61 +1,76 @@
 /**
- * @author Cian Clarke
+ * A proxy for Sencha Touch data stores which retrieves data from FeedHenry's $fh.act() calls.
  *
+ * @author Cian Clarke <cian.clarke@feedhenry.com>
  */
+
 Ext.define('Ext.ux.FHActProxy', {
-  extend: 'Ext.data.proxy.Server',
+    extend: 'Ext.data.proxy.Server',
+    alias: 'proxy.fhactproxy',
 
-  requires: ['Ext.util.MixedCollection', 'Ext.Ajax'],
-  alias: 'proxy.fhactproxy',
+    config: {
 
-  config: {
+        // The name of the act method to call.
+        act: null,
+
+        // The requests body which will be sent to the act call (must be an object).
+        req: null,
+
+        // We don't want to include some of the default parameters along with our request.
+        pageParam: false,
+        startParam: false,
+        limitParam: false,
+        groupParam: false,
+        sortParam: false,
+        filterParam: false,
+        directionParam: false,
+        enablePagingParams: false,
+
+        // The default timeout of 30 seconds is far too long for a good mobile user experience.
+        timeout: 15000
+    },
+
     /**
-     * @property {Object} act
-     * Name of the serverside function to call
+     * Performs the $fh.act() request.
+     *
+     * @param {Ext.data.Operation} operation The Ext.data.Operation object.
+     * @param {Function} callback The callback function to call when the Operation has completed.
+     * @param {Object} scope The scope in which to execute the callback.
      */
-    act: undefined,
+    doRequest: function doRequest(operation, callback, scope) {
+        var that = this,
+            actId = this.getAct(),
+            req = this.getReq(),
+            localItem;
 
-    /**
-     * @property {Object} req
-     * Request body to be sent with any act call
-     */
-    req: undefined
-  },
+        // By convention if we get a string as the request, it's implied that we should check
+        // localStorage for a matching request object.
+        if (typeof req === 'string') {
+            localItem = localStorage.getItem(req);
 
-  /**
-   * Performs FeedHenry request.
-   */
-  doRequest: function(operation, callback, scope) {
-    var writer  = this.getWriter(),
-    me = this,
-    actId = this.getAct(),
-    req = this.getReq();
-
-    if (typeof req === "string"){
-      $fh.data({act: 'load', key: req}, function(res){
-        req = (res && res.val) ? res.val  : '';
-        req = JSON.parse(req);
-        doAct(actId, req);
-      })
-    }else{
-      doAct(actId, req);
-    }
-
-    function doAct(actId, req){
-      $fh.act( {
-        'act' : actId,
-        'req':req
-      },
-      function(res){
-        // Check for a .data or .records property if what we got back isn't an array
-        if (!res.length){
-          res = res.data || res.records;
+            // localStorage will return null if the key doesn't exist, so we must normalise it.
+            if (!localItem) localItem = '';
+            localItem = JSON.parse(localItem);
         }
-        me.processResponse(true, operation, null, res, callback, scope);
-      },
-      function(err){
-        me.processResponse(false, operation, null, err, callback, scope);
-      });
+
+        doAct(actId, req);
+
+        function doAct(actId, req) {
+            $fh.act({
+                    'act': actId,
+                    'req': req
+                },
+                function(res) {
+
+                    // Check for a 'data' or 'records' property if what we got back isn't an array.
+                    if (!Array.isArray(res)){
+                        res = res.data || res.records;
+                    }
+                    that.processResponse(true, operation, null, res, callback, scope);
+                },
+                function(err){
+                    that.processResponse(false, operation, null, err, callback, scope);
+            });
+        }
     }
-  }
 });
